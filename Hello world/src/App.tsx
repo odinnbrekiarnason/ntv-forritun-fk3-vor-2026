f494b43dfdf732c13c013aea1a8a49770a70a972
odinn breki <odinnbreki@outlook.com>
2026-02-19 20:27:36 +0000

    x

--- Hello world/src/App.tsx
+++ Hello world/src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [myName, setName] = useState('UserName');
   const [email, setEmail] = useState('Email');
-  const [] = useState();
 
   return (
     <>
