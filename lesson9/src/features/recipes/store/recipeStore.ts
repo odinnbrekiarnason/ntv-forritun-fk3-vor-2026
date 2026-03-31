e7ef138eb665dab8ea01fe8133b1bea6b9075ada
odinnbrekiarnason <odinnbreki@outlook.com>
2026-03-31 17:59:15 +0000

    fix

--- lesson9/src/features/recipes/store/recipeStore.ts
+++ lesson9/src/features/recipes/store/recipeStore.ts
@@ -1,1 +1,9 @@
 
+interface RecipeStore {
+  Name: string;
+  UpdateRecipe: () => void;
+  UpdateIMG: (url: string) => void;
+  GetIngredients: (id: string) => void;
+  GetMeasurements: (id: string) => void;
+  
+}
