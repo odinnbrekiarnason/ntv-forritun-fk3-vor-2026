399457e7c8f4ccc44cc4a78282840559ba63beca
odinnbrekiarnason <odinnbreki@outlook.com>
2026-04-21 20:15:44 +0000

    a

--- lesson13/src/components/Counter.test.tsx
+++ lesson13/src/components/Counter.test.tsx
@@ -1,1 +1,42 @@
-describe('Counter', () => {});
+import { Counter } from "./Counter";
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+
+describe('Counter', async () => {
+  const user = userEvent.setup()
+
+  it('Should render counter', async () => {
+    render(<Counter/>);
+
+    expect(screen.findByRole('button')).toBeTruthy()
+  });
+  
+  it('Should decrement', async () => {
+    render(<Counter/>);
+
+    await user.click(screen.getByRole('button', {name: /Minnka/i}))
+
+    expect(screen.getByText('-1')).toBeTruthy();
+  })
+
+  it('Should increment', async () => {
+    render(<Counter/>);
+
+    await user.click(screen.getByRole('button', {name: /Hækka/i}));
+
+    expect(screen.getByText('1')).toBeTruthy();
+ })
+
+ it('Should reset', async () => {
+  render(<Counter/>);
+
+  await user.click(screen.getByRole('button', {name: /Hækka/i}))
+
+  expect(screen.getByText('1')).toBeTruthy()
+
+  await user.click(screen.getByRole('button', {name: /Endurstilla/i}));
+
+  expect(screen.getByText('0')).toBeTruthy()
+ })
+});
--- lesson13/src/components/Greeting.test.tsx
+++ lesson13/src/components/Greeting.test.tsx
@@ -1,1 +1,21 @@
-describe('Greeting', () => {});
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Greeting } from "./Greeting";
+
+describe('Greeting', async () => {
+  const user = userEvent.setup();
+
+  it('Should render', () => {
+    render(<Greeting/>);
+    expect(screen.findByRole('textbox', {name: /Nafn/i})).toBeTruthy()
+  })
+
+  it('Should say "Halló, {userInput}!"', async () => {
+    render(<Greeting/>);
+
+    await user.keyboard('odinn')
+    await user.click(screen.getByRole('button', {name: /Senda/i}))
+
+    expect(screen.findByText('Halló, odinn!')).toBeTruthy()
+  })
+});
