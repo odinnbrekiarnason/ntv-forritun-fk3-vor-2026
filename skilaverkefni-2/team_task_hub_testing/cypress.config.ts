e0534cc7ea39ffbbf1d5495dd8ed035e6dfded33
odinnbrekiarnason <odinnbreki@outlook.com>
2026-05-03 22:23:34 +0000

    fix

--- skilaverkefni-2/team_task_hub_testing/cypress.config.ts
+++ skilaverkefni-2/team_task_hub_testing/cypress.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:5173/',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: 'cypress/e2e/**/*.cy.{ts,tsx}',
+    supportFile: 'cypress/support/e2e.ts'
   },
 });
 
