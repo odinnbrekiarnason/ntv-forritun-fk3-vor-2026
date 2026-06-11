a37dac44e6d18170a882842c0ac6d7fc84274236
odinnbrekiarnason <odinnbreki@outlook.com>
2026-06-11 21:28:44 +0000

    fix

--- Lokaverkefni/src/Features/API/controllers/userCTRL/userController.ts
+++ Lokaverkefni/src/Features/API/controllers/userCTRL/userController.ts
@@ -4,19 +4,19 @@ import pool from "@config/db";
 export const inputUserInDB = async (_req: Request, _res: Response, _next: NextFunction) => {
   try {
     const payload = _req.body?.user ?? _req.body;
-    const { id, username, email } = payload;
+    const { clerk_uid, username, email } = payload;
 
-    if (!id || !username || !email) {
+    if (!clerk_uid || !username || !email) {
       _res.status(403).json({ error: "User not authenticated" });
       _next();
     } else {
 
-      const existingUser = await pool.oneOrNone('Select * from users where id = $1', [id]);
+      const existingUser = await pool.oneOrNone('Select * from users where clerk_uid = $1', [clerk_uid]);
 
       if (existingUser) {
         _res.status(403).json({ error: "User already exists in database" });
       } else {
-        await pool.none('Insert into users (id, username, email) values ($1, $2, $3)', [id, username, email]);
+        await pool.none('Insert into users (clerk_uid, username, email) values ($1, $2, $3)', [clerk_uid, username, email]);
         _res.status(200).json({ message: "User inserted into database" });
       }
     }
@@ -34,7 +34,7 @@ export const getUserById = async (_req: Request, _res: Response, _next: NextFunc
       _res.status(403).json({ error: "Invalid user ID" });
       _next();
     } else {
-      const result = await pool.oneOrNone('Select * from users where id = $1', [userId]);
+      const result = await pool.oneOrNone('Select * from users where clerk_uid = $1', [userId]);
       
       if (!result) {
         _res.status(404).json({ error: "User not found"});
