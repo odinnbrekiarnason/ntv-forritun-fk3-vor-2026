f5e666716d04769e220c1e916c7c832fc1ceec2e
odinnbrekiarnason <odinnbreki@outlook.com>
2026-06-15 20:30:50 +0000

    smol fix 2

--- Lokaverkefni/src/Features/API/schemas/UserSchema.ts
+++ Lokaverkefni/src/Features/API/schemas/UserSchema.ts
@@ -4,7 +4,7 @@ export const UserInputSchema = z
 	.object({
 		clerk_uid: z.string().nonempty('clerk_uid Required'),
 		username: z.string().nonempty('username Required'),
-		email: z.string().nonempty('email is required').email('Invalid email format'),
+		email: z.string().nonempty('email is required').includes('@', 'email must contain @'),
 		shop_role: z.string(),
 		firstName: z.string().min(1, 'firstName Required').optional(),
 		firstname: z.string().min(1, 'firstname Required').optional(),
